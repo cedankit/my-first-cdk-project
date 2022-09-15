4d1573c5bf060c18497dce13b0149225d2caba3d
Ankit Tiwari <ankittiwari@cedcommerce.com>
2022-09-15 16:54:19 +0530

    First Commit

--- my_first_project/my_first_project_stack.py
+++ my_first_project/my_first_project_stack.py
@@ -1,9 +1,15 @@
+import enum
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_s3 as _s3,
+
+
+
     # aws_sqs as sqs,
 )
 from constructs import Construct
+
 
 class MyFirstProjectStack(Stack):
 
@@ -11,6 +17,13 @@ class MyFirstProjectStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        _s3.Bucket(
+            self,
+            "myBucketId",
+            bucket_name="cdk-ankit-35",
+            versioned=True,
+            encryption=_s3.BucketEncryption.KMS_MANAGED
+        )
 
         # example resource
         # queue = sqs.Queue(
